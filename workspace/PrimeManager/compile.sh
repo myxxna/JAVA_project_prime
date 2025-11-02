53df3462434f2936e229092c1d655c7b57fbe96c
kang <tdop8621@naver.com>
2025-11-02 15:56:38 +0900

    AppLauncher change

---
+++ workspace/PrimeManager/compile.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+javac -d /Users/ksw/Prime/workspace/PrimeManager/bin --module-path "/Users/ksw/Downloads/javafx-sdk-21.0.8/lib" --add-modules javafx.controls,javafx.graphics /Users/ksw/Prime/workspace/PrimeManager/src/AppLauncher.java
