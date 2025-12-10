7302d19eafba5646481385ba46c571db39e341ae
j-ihyun <sjhtwg1@gmail.com>
2025-12-11 08:43:57 +0900

    입실, 예약, 퇴실/연장하기, 좌석 상태 변화

--- workspace/PrimeManager/src/interfaces/IUserDAO.java
+++ workspace/PrimeManager/src/interfaces/IUserDAO.java
@@ -1,8 +1,10 @@
 package interfaces;
 
 import model.User;
+import java.util.Optional;
 
 public interface IUserDAO {
 //    User login(String studentId);
+	Optional<User> findById(int userId);
     User login(String studentId, String password);
 }
